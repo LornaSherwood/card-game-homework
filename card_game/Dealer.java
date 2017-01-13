4130fd3fc64fc2fb353ce0b57b5007422a96d78f
Lorna Sherwood <lornasherwood@gmail.com>
2017-01-13 16:50:00 +0000

    add dealer gets card from deck

--- card_game/Dealer.java
+++ card_game/Dealer.java
@@ -2,22 +2,42 @@ package card_game;
 import behaviours.*;
 import java.util.*;
 
-public class Dealer implements DealtToAble {
-  Deck deck;
+public class Dealer {
+  private String name;
   ArrayList<DealtToAble> players; // array of dealt to able players called players
+  Dealable deck;
 
   public Dealer(){
+    this.name = name;
     this.players = new ArrayList<DealtToAble>();
-  }
-
-  public void setDeck(Deck deck){
     this.deck = deck;
   }
 
-  public Deck getDeck(){
+  public void setDeck(Dealable deck){
+    this.deck = deck;
+  }
+
+  public Dealable hasDeck(){
     return this.deck;
   }
 
+  public void getPlayer(DealtToAble player){
+    players.add(player);
+  }
+
+  public int countPlayers(){
+    return players.size();
+  }
+
+  public Populatable getCard(){
+    Deck returnedDeck = (Deck) deck;
+    return returnedDeck.showCard(0);
+    
+  }
+
+  /*public void dealCard(){
+    DealtToAble.hand.add(Dealable.pack.remove(0)); // take card from deck, put card in player hand 
+  }*/
 
 
 }
--- card_game_specs/DealerTest.java
+++ card_game_specs/DealerTest.java
@@ -8,6 +8,7 @@ public class DealerTest {
   Player player;
   Deck deck;
   Deck deck1;
+  Card card;
 
 
   @Before
@@ -16,23 +17,49 @@ public class DealerTest {
     player = new Player("Rory");
     deck = new Deck("Standard", 52);
     deck1 = new Deck("Uno", 45);
+    card = new Card("Standard", 3, "Hearts");
     
   }
 
   @Test
   public void startsWithNoDeck(){
-  assertEquals(null, dealer.getDeck());
+    assertEquals(null, dealer.hasDeck());
 }
 
   @Test
   public void canSetDeck(){
     dealer.setDeck(deck1);
-    assertEquals(deck1, dealer.getDeck());
+    assertEquals(deck1, dealer.hasDeck());
   }
 
-  /*@Test
+  @Test
+  public void hasDeck(){
+    assertEquals(deck, dealer.hasDeck());
+
+  }
+
+  @Test
+  public void canGetPlayer(){
+    dealer.getPlayer(player);
+    assertEquals(1, dealer.countPlayers());
+  }
+
+  @Test
+  public void canGetCard(){
+    assertEquals(card, dealer.getCard());
+  }
+
+
+/*
+@Test
   public void canDealCard(){
 
+    deck.getCard(card);
+    dealer.setDeck(deck);
+    dealer.getPlayer(player);
+    dealer.dealCard(card);
+    assertEquals(0, deck.packCount());
+    assertEquals(1, player.countHand());
   }*/
 
 
