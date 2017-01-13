f86e9a9e0299b97314c67f2a3b0e0ddd34d184eb
Lorna Sherwood <lornasherwood@gmail.com>
2017-01-13 09:37:24 +0000

    initialize player

---
+++ card_game/Player.java
@@ -0,0 +1,17 @@
+package card_game;
+import behaviours.*;
+import java.util.*;
+
+public class Player {
+
+  String name;
+
+  public Player(String name) {
+    this.name = name;
+  }
+
+  public String getName(){
+    return this.name;
+  }
+
+}
